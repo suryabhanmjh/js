c4347befcd1f942a17025b505c5ad7ba3a44b274
Suryabhan Singh <suryabhan283808@gmail.com>
2025-03-31 18:21:56 +0530

    refactor js/index.html for improved readability and consistency; add BOM and first.js files for new functionality

---
+++ js/bom.js
@@ -0,0 +1,15 @@
+function sur(){
+//BOM - Browser Object Model
+
+   // window.alert("Hello, Surya!");
+    // let confirms= window.confirm("Are you sure?");
+    // console.log(confirms);
+
+    let prompts= window.prompt("Enter your name:");
+
+    // alert me dikhane ke liye
+
+    window.alert("Hello, " + prompts + "! Welcome to the BOM tutorial.");
+}
+
+sur()
