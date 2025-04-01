448f4507959bacb8e7bf6ab558e3604f927760d5
Suryabhan Singh <suryabhan283808@gmail.com>
2025-04-01 18:01:40 +0530

    refactor bom.js to comment out user interaction prompts and add sum calculation logic

--- js/bom.js
+++ js/bom.js
@@ -19,14 +19,25 @@ function sur() {
     //     alert("Thanks for visiting my website!");
     // }
 
+    //add the two numbers using prompt and alert
+        //   parseInt(), and parseFloat() Number() are used to convert string to number
+    let num1 = parseFloat(prompt("Enter first number:"));
+    let num2 = parseFloat(prompt("Enter second number:"));
+    let sum = num1 + num2;
+    alert("The sum is: " + sum);
+
+
+
     // Task 1: Ask the user of their age and check if they are 18 or older using a ternary operator.
     // let age = prompt("Enter your age:");
     // alert(age >= 18 ? "You are an adult." : "You are a minor.");
+
 
     //  Task 2: Ask if the user has completed a course and has a certificate
     // let completed = confirm("Have you completed the course?");
     // let certificate = confirm("Do you have a certificate?");
     // alert(completed && certificate ? "Completed" : "Not completed");
+
 
     // Task 3: Ask the user for the price of an item and check if they have a discount coupon
     // let price = parseFloat(prompt("Enter the price of the item:"));
@@ -34,9 +45,10 @@ function sur() {
     // let finalPrice = hasCoupon ? price * 0.9 : price;
     // alert( finalPrice.toFixed(2) );
 
+
     // Task 4: Ask the user for their exam score and determine if they passed or failed
-    let score = parseInt(prompt("Enter your exam score:"));
-    alert(score > 33 ? "Pass" : "Fail");
+    // let score = parseInt(prompt("Enter your exam score:"));
+    // alert(score > 33 ? "Pass" : "Fail");
 
 
 
