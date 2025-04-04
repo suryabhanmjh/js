cc30b9fe5c3e594383e8f625f9434eda5a258099
Suryabhan Singh <suryabhan283808@gmail.com>
2025-04-04 18:26:04 +0530

    add: enhance Array.js with additional array manipulation examples and concatenation

--- js/Array.js
+++ js/Array.js
@@ -7,3 +7,22 @@ console.log(Fruit.length); //print length of array of fruits
 console.log(Numbers.length); //print length of array of numbers
 Fruit.pop(); //remove last element of array of fruits
 console.log(Fruit); //print array of fruits after removing last element
+Fruit.push("Pineapple","papaya"); //add element to array of fruits
+Fruit.pop(); 
+Fruit.pop();//remove last element of array of fruits
+
+console.log(Fruit); //print array of fruits after adding element
+
+Fruit.shift(); //remove first element of array of fruits
+console.log(Fruit); //print array of fruits after removing first element
+
+Fruit.unshift("kiwi"); //add element to array of fruits
+console.log(Fruit); //print array of fruits after adding element
+
+//concate method in array
+
+let arr1=[1,2,3,4,5]
+let arr2=[6,7,8,9,10]
+
+let arr3=arr1.concat(arr2) //concatenate two arrays
+console.log(arr3); //print concatenated array
