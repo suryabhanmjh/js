3316ca337326573f7e29e29f3ae0235de9b0da07
Suryabhan Singh <suryabhan283808@gmail.com>
2025-04-05 18:33:24 +0530

    refactor: comment out example code in Array.js for clarity

--- js/Array.js
+++ js/Array.js
@@ -1,41 +1,49 @@
 //Array
-let Fruit=["Apple", "Banana", "Mango", "Orange", "Grapes"] //array of fruits
-let Numbers=[1, 2, 3, 4, 5] //array of numbers
+// let Fruit=["Apple", "Banana", "Mango", "Orange", "Grapes"] //array of fruits
+// let Numbers=[1, 2, 3, 4, 5] //array of numbers
 
-console.log(Fruit [2]); //print array of fruits
-console.log(Fruit.length); //print length of array of fruits
-console.log(Numbers.length); //print length of array of numbers
-Fruit.pop(); //remove last element of array of fruits
-console.log(Fruit); //print array of fruits after removing last element
-Fruit.push("Pineapple","papaya"); //add element to array of fruits
-Fruit.pop(); 
-Fruit.pop();//remove last element of array of fruits
+// console.log(Fruit [2]); //print array of fruits
+// console.log(Fruit.length); //print length of array of fruits
+// console.log(Numbers.length); //print length of array of numbers
+// Fruit.pop(); //remove last element of array of fruits
+// console.log(Fruit); //print array of fruits after removing last element
+// Fruit.push("Pineapple","papaya"); //add element to array of fruits
+// Fruit.pop(); 
+// Fruit.pop();//remove last element of array of fruits
 
-console.log(Fruit); //print array of fruits after adding element
+// console.log(Fruit); //print array of fruits after adding element
 
-Fruit.shift(); //remove first element of array of fruits
-console.log(Fruit); //print array of fruits after removing first element
+// Fruit.shift(); //remove first element of array of fruits
+// console.log(Fruit); //print array of fruits after removing first element
 
-Fruit.unshift("kiwi"); //add element to array of fruits
-console.log(Fruit); //print array of fruits after adding element
+// Fruit.unshift("kiwi"); //add element to array of fruits
+// console.log(Fruit); //print array of fruits after adding element
 
 //concate method in array
 
-let arr1=[1,2,3,4,5]
-let arr2=[6,7,8,9,10]
+// let arr1=[1,2,3,4,5]
+// let arr2=[6,7,8,9,10]
 
-let arr3=arr1.concat(arr2) //concatenate two arrays
-console.log(arr3); //print concatenated array
-console.log(arr1); //print first array
-console.log(arr2); //print second array
+// let arr3=arr1.concat(arr2) //concatenate two arrays
+// console.log(arr3); //print concatenated array
+// console.log(arr1); //print first array
+// console.log(arr2); //print second array
 
 //slice method in array
-let arr4=[1,2,3,4,5,6,7,8,9,10] //array of numbers
-let arr5=arr4.slice(2,5) //slice array from index 2 to 5
-console.log(arr5); //print sliced array
+// let arr4=[1,2,3,4,5,6,7,8,9,10] //array of numbers
+// let arr5=arr4.slice(2,5) //slice array from index 2 to 5
+// console.log(arr5); //print sliced array
 
 //splice method in array
 // let arr6=[1,2,3,4,5,6,7,8,9,10] //array of numbers
 // let arr7=arr6.splice(2,6,"apple") //splice array from index 2 to 3
 // console.log(arr7); //print spliced array
 
+// let arr=[1,2,6,4,9,7,8,9,] //array of numbers
+
+// console.log(arr.sort()); //print array of numbers
+
+//desending order
+let arr=[1,2,7,3,6,8,9] //array of numbers
+//nsole.log(arr.sort(function(a,b){return b-a})); //print array of numbers in desending order
+console.log(arr.sort().reverse)
