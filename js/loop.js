930b685ef035d002f39b6867a7de3a13ed072c22
Suryabhan Singh <suryabhan283808@gmail.com>
2025-04-05 15:14:55 +0530

    add: create loop.html and loop.js with initial array looping example

---
+++ js/loop.js
@@ -0,0 +1,11 @@
+// One way to loop through an array, is using a for loop:
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+document.getElementById("demo").innerHTML = text;
