6ebb2bd8cb46e48e58fddda2403bc6508562b295
Suryabhan Singh <suryabhan283808@gmail.com>
2025-04-12 18:13:40 +0530

    fix: update button class and function name for consistency in form validation

--- js/form.js
+++ js/form.js
@@ -1,6 +1,15 @@
-let val=()=>{
+let Val=()=>{
     
     let inputname= document.querySelector("#name").value
+    let inputnumber=document.querySelector("#number").value
+    let inputemail=document.querySelector("#email").value
+    let inputpass=document.querySelector("#pass").value
+    let inputcpass=document.querySelector("#cpass").value
+    let errname=document.querySelector("#errname")
+    let erremail=document.querySelector("#erremail")
+    let errpass=document.querySelector("#errpass")
+    let errcpass=document.querySelector("#errcpass")
+    let errnumber=document.querySelector("#errnumber")
      
     // let errname= document.querySelector("#errname")
     // if(inputname.length==0){
@@ -15,36 +24,47 @@ let val=()=>{
      return false
     }
  
-    let inputnumber=document.querySelector("#number").value
- 
-    let errnumber=document.querySelector("#errnumber")
-    if(inputnumber==""){
+    
+    else if(inputnumber==""){
        errnumber.innerHTML="Please enter your number!";
        errnumber.style.color="red"
        return false
     }
+
+    else if(isNaN(inputnumber)){
+         errnumber.innerHTML="Please enter only number!";
+         errnumber.style.color="red"
+         return false
+     }
+     else if(inputnumber.length!=10){
+            errnumber.innerHTML="Please enter only 10 digits only!";
+            errnumber.style.color="red"
+            return false
+        }
  
-    let inputemail=document.querySelector("#email").value
     
-    if(inputemail==""){
+    
+    else if(inputemail==""){
        erremail.innerHTML="Please enter your email!";
        erremail.style.color="red"
        return false
     }
     
-    let inputpass=document.querySelector("#pass").value
     
-    if(inputpass==""){
+    
+    else if(inputpass==""){
        errpass.innerHTML="Please enter your password!";
        errpass.style.color="red"
        return false
     }
  
-    let inputcpass=document.querySelector("#cpass").value
+    
  
-    if(inputcpass==""){
+    else if(inputcpass==""){
        errcpass.innerHTML="Please enter your confirm password!"
        errcpass.style.color="red"
        return false
     }
+
+
  }
