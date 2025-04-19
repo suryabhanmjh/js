67468f42b40864dbe33b101815dabdf5e6264126
Suryabhan Singh <suryabhan283808@gmail.com>
2025-04-19 18:07:35 +0530

    add: expand date.js with detailed date and time logging examples

--- js/date.js
+++ js/date.js
@@ -1,2 +1,17 @@
 let MyDate = new Date();
 console.log(MyDate);
+console.log(MyDate.getFullYear()); // 2025
+console.log(MyDate.getMonth()+1); // 0-11
+console.log(MyDate.getDate()); // 1-31
+console.log(MyDate.getDay()+1); // 0-6
+console.log(MyDate.getHours()); // 0-23
+console.log(MyDate.getMinutes()); // 0-59
+console.log(MyDate.getSeconds()); // 0-59
+console.log(MyDate.getMilliseconds()); // 0-999
+// saturday print
+//  console.log(MyDate.getDay()===6 ? "Saturday" : "Not Saturday");
+
+//  second method in arr all days
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[MyDate.getDay()]); // Saturday
+
